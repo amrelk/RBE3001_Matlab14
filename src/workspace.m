b428296ff068801c7ca9d1bdf6ea46aec1aede4e
Evelyn Maude <evelyn@evelynmau.de>
2022-02-02 10:49:28 -0500

    updated workspace to be slightly more accurate

--- src/workspace.m
+++ src/workspace.m
@@ -3,7 +3,7 @@ kine = Kinematics();
 t = zeros(80000, 3);
 n = 1;
 
-for q1 = linspace(-180, 180, 50)
+for q1 = linspace(-90, 90, 50)
     for q2 = linspace(-50, 98, 40)
         for q3 = linspace(-105, 74, 40)
             T = kine.fk3001([q1 q2 q3]);
