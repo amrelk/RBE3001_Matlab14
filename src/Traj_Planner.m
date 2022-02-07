37246d1b1014d3d6f080a1e51e648d78c0c747b6
Ulfin85 <92341475+Ulfin85@users.noreply.github.com>
2022-02-07 16:09:37 -0500

    Added quintic function

--- src/Traj_Planner.m
+++ src/Traj_Planner.m
@@ -11,6 +11,22 @@ classdef Traj_Planner
                v1];
             A=flip(M\Q);
         end
+
+        function B = quintic_traj(self, p0, p1, v0, v1, a0, a1, t0, t1)
+            M=[1 t0 t0^2 t0^3 t0^4 t0^5;
+               0 1 2*t0 3*t0^2 4*t0^3 5*t0^4;
+               0 0 2 6*t0 12*t0^2 20*t0^3;
+               1 t1 t1^2 t1^3 t1^4 t1^5;
+               0 1 2*t1 3*t1^2 4*t1^3 5*t1^4;
+               0 0 2 6*t1 12*t1^2 20*t1^3];
+            Q=[p0;
+               v0;
+               a0;
+               p1;
+               v1;
+               a1];
+            B=flip(M\Q);
+        end
     end
 
 end
